7144aaec32381694017706cca54e6b6d85e3d0a8
bobnorigun <bobthedesigner@gmail.com>
2019-10-29 17:44:36 +0900

    form field title remove

--- blog/forms.py
+++ blog/forms.py
@@ -6,6 +6,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+        widgets = {
+        	'title' : forms.TextInput(attrs={'placeholder': '제목'}),
+        	'text' : forms.Textarea(attrs={'placeholder': '내용'})
+        }
         
 class CommentForm(forms.ModelForm):
 
