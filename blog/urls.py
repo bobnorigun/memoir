3573fce741a87499654c552fed60378b9357fbb7
bobnorigun <bobthedesigner@gmail.com>
2020-03-01 01:25:05 +0900

    event page add

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -8,10 +10,11 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('recent/', views.post_recent_list, name='post_recent_list'),
+    path('recent/', views.post_recent_list, name='post_recent_list'),    
+    path('event/', views.post_event, name='post_event'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     path('post/search/', views.post_search, name='post_search'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,9 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_event(request):
+    return render(request, 'blog/post_event.html')
+
 def post_recent_list(request):
     modified = Post.objects.filter(published_date__lte=timezone.now()).order_by('-last_modified')
     return render(request, 'blog/post_recent_list.html', {'modified': modified})
