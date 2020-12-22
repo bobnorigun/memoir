5957058a77b94e1354fe30c193e6ca11b414200b
bobnorigun <39334727+bobnorigun@users.noreply.github.com>
2020-12-22 14:51:19 +0900

    root change
    papamob on blog off

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'),
+    path('blog/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('recent/', views.post_recent_list, name='post_recent_list'),
+    path('', views.post_event, name='post_event'),
     path('event/', views.post_event, name='post_event'),
     path('app_later/', views.post_app_later, name='post_app_later'),
     path('app_memolang/', views.post_app_memolang, name='post_app_memolang'),
