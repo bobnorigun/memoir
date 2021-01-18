86f876af635061ba83965a6419d30c4b7832ee7c
bobnorigun <39334727+bobnorigun@users.noreply.github.com>
2021-01-19 04:36:21 +0900

    catalog app added
    catalog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    #'basicform',
+    'catalog.apps.CatalogConfig', #added app
     #'papamob',
     #'distsum',
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,10 +20,23 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    #path('basicform/', include('basicform2.urls')),
+    #path('', include('blog.urls')),
+    path('catalog/', include('catalog.urls')),
     #path('papamob/', include('papamob.urls')),
     #path('distsum/', include('distsum.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
+
+#리다이렉트를 사용하는 것과 그냥 패쓰의 ''에 include('catalog.urls')지정하는게 달라?
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
