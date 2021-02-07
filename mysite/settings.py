cb2e836afcdf29abac16610fa5ed91cce7f12b34
bobnorigun <39334727+bobnorigun@users.noreply.github.com>
2021-02-08 02:29:05 +0900

    logout crash
    in papamob app

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,5 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
-#LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
