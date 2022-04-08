080cb57633b5b8ca4cb89dafa5a17f6c6fe1632e
bobnorigun <39334727+bobnorigun@users.noreply.github.com>
2022-04-08 16:10:36 +0900

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
 admin.site.register(Comment)
+
+#@admin으로 시작하면 list_display이하 옵션을 쓸 수 있음
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'last_modified')
