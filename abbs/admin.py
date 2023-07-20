1ce659afb820e384443a2878cfa295989f5af96b
bobnorigun <39334727+bobnorigun@users.noreply.github.com>
2023-07-20 22:51:59 +0900

    comments

--- abbs/admin.py
+++ abbs/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Genre, Book, PapaAbb
+from .models import Genre, Book, PapaAbb, GetWeb, Comment
 
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(PapaAbb)
+admin.site.register(GetWeb)
+admin.site.register(Comment)
 
 # 어드민도 뷰를 정의해서 어드민페이지에서 디테일한 내용이나 관련 편집도 가능. 그너데 이건 나중에.
